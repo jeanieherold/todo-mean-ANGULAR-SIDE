71ae64ee4926be21462d858355245ae7a47f0b0f
jeanieherold <jeaniemherold68@gmail.com>
2016-11-14 23:17:39 -0500

    on a wing and a prayer

--- front-end/src/app/index.module.js
+++ front-end/src/app/index.module.js
@@ -5,12 +5,14 @@ import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
 import { AuthController } from './auth/auth.controller';
+import { CompareToDirective } from './directives/compareTo.directive';
 import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
 import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
 import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
 
-angular.module('appFrontend', ['ui.router', 'ui.bootstrap', 'toastr'])
+angular.module('appFrontend', ['ui.router', 'ui.bootstrap', 'toastr', 'satellizer'])
+  .constant('API_URL', 'http://localhost:5000/')
   .constant('malarkey', malarkey)
   .constant('moment', moment)
   .config(config)
@@ -22,3 +24,4 @@ angular.module('appFrontend', ['ui.router', 'ui.bootstrap', 'toastr'])
   .controller('AuthController', AuthController)
   .directive('acmeNavbar', NavbarDirective)
   .directive('acmeMalarkey', MalarkeyDirective);
+  .directive('compareTo', CompareToDirective);
